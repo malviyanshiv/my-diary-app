cc60ed23e1d61e947fc7fe233b69e1448e7e2ea5
Shiv Shankar Singh <malviyanshiv@gmail.com>
2020-03-16 19:34:04 +0530

    added editor functionality and linked it statically

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
-/* variables declarations */
-let toolbar = document.getElementsByClassName("toolbar")[0];
-let sticky = toolbar.offsetTop;
-
-
-/* Event Registrations */
-window.onscroll = function() { windowScrollHandler()};
-
-
-/* Event handlers */
-
-function windowScrollHandler(){
-    if (window.pageYOffset >= 30 )
-        toolbar.classList.add("sticky");
-    
-    else
-        toolbar.classList.remove("sticky");
-}
-
 function hideElement(className){
     let ele = document.getElementsByClassName(className);
     for(let i = 0; i < ele.length; i++)
@@ -38,25 +19,12 @@ function onclickHandler(current, toshow, tohide){
     showElement(toshow);
 }
 
+function loadEditor(){
+    let editor = document.getElementById("editor-container");
+    editor.getElementsByClassName("page")[0].innerHTML = document.getElementById("diary-page").innerHTML;
+    editor.classList.remove("hide");
+}
 
-/* --------------------------- EDITOR ------------------------------- */
-
-function showToolbars(cur, toolbars){
-    let done = cur.classList.contains("toolbar__item-active");
-    let tmp = document.getElementsByClassName("toolbar-active");
-    while( tmp.length > 0 )
-        tmp[0].classList.remove("toolbar-active");
-    
-    if( done ){
-        cur.classList.remove("toolbar__item-active");
-        return;
-    }
-    
-    let act = document.getElementsByClassName("toolbar__item-active");
-    while( act.length > 0 )
-        act[0].classList.remove("toolbar__item-active");
-        
-    cur.classList.add("toolbar__item-active");
-    for(let i = 0; i < toolbars.length; i++)
-        document.getElementById(toolbars[i]).classList.add("toolbar-active");
+function hideEditor(){
+    document.getElementById("editor-container").classList.add("hide");
 }
